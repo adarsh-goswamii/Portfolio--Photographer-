8b1f8bb5dd55fdf17c8ea60e0711ac796846ae92
Adarsh Goswami <adarsh010120@gmail.com>
2021-09-03 16:03:25 +0530

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+// var image= document.querySelector(".hero-img");
+// image= image.children[0];
+// // console.log(image);
+
+// image.addEventListener('mousehover', ()=> {
+//     image.src= '/img/aditya.jpg';
+//     console.log("hii in", image.src);
+// });
+
+// image.addEventListener('mouseout', ()=> {
+//     image.src= '/img/aditya_bw.jpg';
+//     console.log("hii out", image.src);
+// });
