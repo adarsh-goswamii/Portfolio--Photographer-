912165e2b56bf1407b9bf8656f25aa78cdb5a74e
Adarsh Goswami <adarsh010120@gmail.com>
2021-09-03 19:33:10 +0530

    portraits and nature slide added

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,27 @@
-// var image= document.querySelector(".hero-img");
-// image= image.children[0];
-// // console.log(image);
+var pages= document.querySelectorAll(".page");
+var left_swipe= document.querySelector(".left-slide");
+var right_swipe= document.querySelector(".right-slide");
+console.log(pages);
+var curr_page= 0;
 
-// image.addEventListener('mousehover', ()=> {
-//     image.src= '/img/aditya.jpg';
-//     console.log("hii in", image.src);
-// });
+right_swipe.addEventListener("click", ()=> {
+    console.log("triggered");
+    for(var i= 0;i<pages.length;i++) {
+        if(!pages[i].classList.contains("vanish"))
+        pages[i].classList.add("vanish");
+    }
+    curr_page= (curr_page+1)%pages.length;
+    console.log(pages[curr_page]);
+    pages[curr_page].classList.remove("vanish");
+});
 
-// image.addEventListener('mouseout', ()=> {
-//     image.src= '/img/aditya_bw.jpg';
-//     console.log("hii out", image.src);
-// });
+left_swipe.addEventListener("click", ()=> {
+    console.log("triggered");
+    for(var i= 0;i<pages.length;i++) {
+        if(!pages[i].classList.contains("vanish"))
+        pages[i].classList.add("vanish");
+    }
+    curr_page= (curr_page-1+ pages.length)%pages.length;
+    console.log(pages[curr_page]);
+    pages[curr_page].classList.remove("vanish");
+});
